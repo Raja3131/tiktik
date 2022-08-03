1954c65379f509a33ff07c4b197e05b78f1a744e
RajaVAASITS <kingraja673@gmail.com>
2022-08-03 20:58:50 +0530

    random commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,10 @@
 import type { NextPage } from 'next'
+import axios from 'axios'
 
-const Home: NextPage = () => {
+interface IProps{
+  videos: Array<any>
+}
+const Home: NextPage = ({}) => {
   return (
     <div>
     <h1 className="text-3xl font-bold underline">
@@ -10,5 +14,14 @@ const Home: NextPage = () => {
     </div>
   )
 }
+export const getServerSideProps = async () => {
+  const response = await axios.get('http://localhost:3000/api/post')
+  console.log(response.data)
 
+  return {
+    props: {
+      videos: response.data
+    }
+  }
+}
 export default Home
